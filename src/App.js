72caeb2e84fa69c386c8f667266da950cb9241de
Nhanle28799 <nhanle28799@gmail.com>
2021-04-10 17:48:11 +0700

    Todolist lab2 useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import './App.css'
-import ColorBox from './component/ColorBox/index'
+import TodoList from './component/TodoList/index';
 
 function App() {
+  const [toDoList, setTodoList] = useState([
+    { id: 1, title: 'haha' },
+    { id: 2, title: 'hihi' },
+    { id: 3, title: 'hoho' },
+  ]);
+  function handleTodoClick(todo) {
+    const index = toDoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+    const newTodo = [...toDoList];
+    newTodo.splice(index, 1)
+    setTodoList(newTodo)
+  }
   return (
     <div className="app">
-      <h1>Welcom to scss</h1>
-      <ColorBox />
+      <h1>Welcom to SASS</h1>
+      <TodoList todos={toDoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
