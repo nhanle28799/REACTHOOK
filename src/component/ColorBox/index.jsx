02d65fc7afa2c47fc521935df4d6cfbe84521fdb
Nhanle28799 <nhanle28799@gmail.com>
2021-04-10 17:07:03 +0700

    usetatelab1-clickbox

---
+++ src/component/ColorBox/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+ColorBox.propTypes = {
+
+};
+function getRandomColor() {
+    const COLOR_LIST = ['deeppink', 'green', 'yellow', 'black', 'blue']
+    const randomIndex = Math.trunc(Math.random() * 5);
+    return COLOR_LIST[randomIndex]
+}
+function ColorBox() {
+
+    const [color, setColor] = useState(() => {
+        const initColor = localStorage.getItem('box_color') || 'deeppink'
+        return initColor
+    });
+    function handleBoxClick() {
+        const newColor = getRandomColor();
+        setColor(newColor);
+        localStorage.setItem('box_color', newColor)
+    }
+    return (
+        <div
+            className="color-Box"
+            style={{ backgroundColor: color }}
+            onClick={handleBoxClick}>
+            ColorBox
+        </div>
+    );
+}
+
+export default ColorBox;
